c22e62179203dd860b1b0f137e61996124d5fb1f
teamconvergence <46495005+teamconvergence@users.noreply.github.com>
2019-02-07 09:52:10 -0800

    Add files via upload

---
+++ robot/Gyroscope.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.Timer;
+
+
+/**
+ * Add your docs here.
+ */
+public class Gyroscope extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  public AnalogGyro gyro = new AnalogGyro();
+
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+    gyro = new AnalogGyro(0);
+  }
+}
